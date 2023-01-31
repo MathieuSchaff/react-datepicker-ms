dcb7dc866fde625fc5774bf0934fd3823a9b65ac
Mathieu Schaff <schaff.mathieu93@gmail.com>
2023-01-31 17:53:42 +0100

    changed format

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,25 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { format } from 'date-fns'
-type Props = {
-  value?: number
-}
-const MyCounter = ({ value = 0 }: Props) => {
-  const [counter, setCounter] = useState(value)
 
-  const onMinus = () => {
-    setCounter((prev) => prev - 1)
-  }
-
-  const onPlus = () => {
-    setCounter((prev) => prev + 1)
-  }
-
+const MyCounter = () => {
   return (
     <div data-testid='my-counter'>
-      <h1>Counter: {counter}</h1>
-      {format(new Date(), 'yyyy-MM-dd')}
-      <button onClick={onMinus}>-</button>
-      <button onClick={onPlus}>+</button>
+      <p>{format(new Date(), 'yyyy-MM-dd')}</p>
+      <p>test</p>
     </div>
   )
 }
